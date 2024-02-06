5e83f6ef98d774928f0b1a412a88ebd8c9754e35
zdicken <zdicken@iu.edu>
2024-02-06 17:32:59 -0500

    player reset, betterish rotation

--- Project 1/Assets/Scripts/PlayerController.cs
+++ Project 1/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
+    private Vector3 lastSafePosition;
 
     public float speed;
     public float turnRate;
 
     void Start() {
         rb = this.GetComponent<Rigidbody>();
+        lastSafePosition = this.transform.position;
     }
 
     void FixedUpdate() {
         //randomly change controls??
-        rb.AddForce(new Vector3(speed * Input.GetAxis("Vertical"), 0.0f, 0.0f));
-        Quaternion target = Quaternion.Euler(0, turnRate * Input.GetAxis("Horizontal"), 0);
-        transform.rotation = Quaternion.Slerp(transform.rotation, target, 5.0f);
+        rb.AddRelativeForce(Vector3.forward * -speed * Input.GetAxis("Vertical"));
+        //Quaternion target = Quaternion.Euler(transform.rotation.x, transform.rotation.y + turnRate * Input.GetAxis("Horizontal"), transform.rotation.z);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, 5.0f);
+        transform.Rotate(0, turnRate * Input.GetAxis("Horizontal"), 0);
+
+        //determine if current position is 'safe' and store the value
+        if(this.transform.position.y < -5) {
+            rb.velocity = new Vector3(0, 0, 0);
+            this.transform.position = lastSafePosition;
+        }
     }
 }
